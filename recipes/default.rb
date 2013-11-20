fbe960dd39fa1e673fdf774b62234412f282291b
Stephen Lauck <lauck@opscode.com>
2013-11-20 09:37:21 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,3 @@
 
 include_recipe "emacs"
 include_recipe "pipeline"
-
-
-pipeline_chef_server 'dev' do
- user_pem node['pipeline']['chef_server']['user_pem']
- validation_pem node['pipeline']['chef_server']['validation_pem']
- url node['pipeline']['chef_server']['url']
- action :create
-end
